95d937ae22a654b09bfd41b3ed4f69b46ba36504
Lorenz Hufnagel <hufnagel@kth.se>
2016-07-19 12:18:43 +0200

    Extract slabs for STF

--- pod/matlab_scripts/extract_slice.m
+++ pod/matlab_scripts/extract_slice.m
@@ -1,10 +1,11 @@
 
-[data,lr1,elmap,time,istep,fields,emode,wdsz,etag,header,status] = readnek('./pipe_mode0.f00001');
+[data,lr1,elmap,time,istep,fields,emode,wdsz,etag,header,status] = readnek('./pipe_c_mode0.f00003');
 if status < 0 
   error('Could not read file');
 end
 
-z_val=1.50; %! corresponds to upstream section, exptected to be <= 0
+%z_val=[-2 -1 -.5:0.25:4 5 8 12]; %! corresponds to upstream section, exptected to be <= 0
+z_val=[-2:0.25:14.75]; 
 
 nFpp = 176;
 bent_phi=90*pi/180; % bent section 
@@ -15,30 +16,34 @@ s_ = sin(bent_phi);
 
 data2d = zeros(nFpp,lr1(1)*lr1(2),6);
 
-for e=1:size(data,1)
-  ex = mod(e-1,nFpp)+1;
+for m=1:length(z_val)
 
-  %   - data:   nek5000 data ordered as (iel,inode,[x|y|(z)|u|v|(w)|p|T|s_i])
-  z_unbent = data(e,1,3);
+  for e=1:size(data,1)
+    ex = mod(e-1,nFpp)+1;
 
-  if (abs(bent_phi) > 1e-6)
-    if (z_unbent > 0) 
+    %   - data:   nek5000 data ordered as (iel,inode,[x|y|(z)|u|v|(w)|p|T|s_i])
+    z_unbent = data(e,1,3);
+    c = 1;
+    s = 0;
+
+    if (abs(bent_phi) > 1e-6)
+      if (z_unbent > 0) 
         angle = atan2(z_unbent,data(e,1,1));
-      if (angle <= bent_phi)
-        z_unbent = bent_radius*angle;
-        c = cos(-angle);
-        s = sin(-angle);
-      else
-        z_unbent = c_*z_unbent/s_^2 - data(e,1,1)/s_;
-        z_unbent = z_unbent/(1+c_^2/s_^2) + circumf;
-        c = cos(-bent_phi);
-        s = sin(-bent_phi);
+        if (angle <= bent_phi)
+          z_unbent = bent_radius*angle;
+          c = cos(-angle);
+          s = sin(-angle);
+        else
+          z_unbent = c_*z_unbent/s_^2 - data(e,1,1)/s_;
+          z_unbent = z_unbent/(1+c_^2/s_^2) + circumf;
+          c = cos(-bent_phi);
+          s = sin(-bent_phi);
+        end
       end
     end
-  end
 
   % numerical precission
-  if (abs(z_val - z_unbent)<1e-5)
+  if (abs(z_val(m) - z_unbent)<1e-5)
     for k=1:(lr1(1)*lr1(2))
       data2d(ex,k,1) = c*data(e,k,1)-s*data(e,k,3); %x
       data2d(ex,k,3) = c*data(e,k,4)-s*data(e,k,6); %u
@@ -57,4 +62,5 @@ lr1(3)=1;
 elmap = int32([1:nFpp]);
 
 
-writenek(['slab_mode0.f' num2str(1,'%05d')],data2d,lr1,elmap,time,istep,fields,emode,wdsz,etag);
+writenek(['slab_mode0.f' num2str(m,'%05d')],data2d,lr1,elmap,z_val(m),m,fields,emode,wdsz,etag);
+end
